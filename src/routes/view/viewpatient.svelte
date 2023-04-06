9080ce11c0f0669460ab74c2a7c8a35ccc2a60fe
rahulad12 <rahul077@academiacollege.edu.np>
2023-04-06 18:17:22 +0545

    patient details

--- src/routes/view/viewpatient.svelte
+++ src/routes/view/viewpatient.svelte
@@ -1,137 +1,133 @@
 <script>
     import { onMount } from "svelte";
-    import { paginate, LightPaginationNav } from 'svelte-paginate'
+    // import { paginate, LightPaginationNav } from 'svelte-paginate'
     // array of patients
     let patient_details = [
-        {
-            id: 1,
-            age: 21,
-            gender: "FEMALE",
-            name: "Rahul Adhikari",
-            address: "Bhaktapur",
-            phone: "9876543210",
-            dob: "2001-01-22T00:00:00.000+00:00",
-        },
-        {
-            id: 2,
-            age: 20,
-            gender: "MALE",
-            name: "Jivan Parajuli",
-            address: "Imadol",
-            phone: "9862116777",
-            dob: "2002-08-30T00:00:00.000+00:00",
-        },
-        {
-            id: 3,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 4,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 5,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 6,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 7,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 8,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 9,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 10,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 11,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 12,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
-        {
-            id: 13,
-            age: 33,
-            gender: "MALE",
-            name: "Ramesh Humagain",
-            address: "Birtamod",
-            phone: "9876543210",
-            dob: "1987-04-02T00:00:00.000+00:00",
-        },
+        // {
+        //     id: 1,
+        //     age: 21,
+        //     gender: "FEMALE",
+        //     name: "Rahul Adhikari",
+        //     address: "Bhaktapur",
+        //     phone: "9876543210",
+        //     dob: "2001-01-22T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 2,
+        //     age: 20,
+        //     gender: "MALE",
+        //     name: "Jivan Parajuli",
+        //     address: "Imadol",
+        //     phone: "9862116777",
+        //     dob: "2002-08-30T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 3,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 4,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 5,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 6,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 7,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 8,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 9,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 10,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 11,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 12,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
+        // {
+        //     id: 13,
+        //     age: 33,
+        //     gender: "MALE",
+        //     name: "Ramesh Humagain",
+        //     address: "Birtamod",
+        //     phone: "9876543210",
+        //     dob: "1987-04-02T00:00:00.000+00:00",
+        // },
     ];
 
-    let currentPage = 1;
-    let pageSize = 9;
+    onMount( async ()=>{
 
-    $: paginatedItems = paginate({ patient_details, pageSize, currentPage });
-    // onMount( async ()=>{
-
-    //     let  grantrequest = await fetch('http://54.64.108.224:8080/patients');
-    //     patient_data = await grantrequest.json();
-    //     console.log(patient_data);
-    // });
+        let  grantrequest = await fetch('http://54.64.108.224:8080/patients');
+        patient_details = await grantrequest.json();
+        console.log(patient_data);
+    });
 </script>
 <section class="section flex flex-col justify-center items-center">
     <h1 class="text-center  text-2xl mt-6">Patient list</h1>
@@ -155,13 +151,4 @@
         
         {/each}
     </div>
-    <LightPaginationNav
-    totalItems="{patient_details.length}"
-    pageSize="{pageSize}"
-    currentPage="{currentPage}"
-    limit="{1}"
-    showStepOptions="{true}"
-    on:setPage="{(e) => currentPage = e.detail.page}"
-  />
-
 </section>
